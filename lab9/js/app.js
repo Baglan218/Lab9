56653e7bfd9a1c4d69470b063b69c46d175cc415
Baglan218 <70622427+Baglan218@users.noreply.github.com>
2021-12-05 23:35:03 +0600

    Add files via upload

---
+++ lab9/js/app.js
@@ -0,0 +1,25 @@
+const username = document.getElementById("name")
+const password = document.getElementById("password")
+const form = document.getElementById("form")
+const errorElement = document.getElementById("error")
+
+form.addEventListener("submit" ,(e) => {
+  let messages = []
+  if (username.value === '' || username.value ==null){
+    messages.push("Name is required")
+  }
+  if(password.value.length <=6){
+    messages.push("Password must be longer than 6 character")
+  }
+  if(password.value.length >=20){
+    messages.push("Password must be less than 20 character")
+  }
+  if (password.value === "password" ){
+    messages.push("Password cannot be password")
+  }
+  if (message.length >0){
+   e.preventDefault()
+   errorElement.innerText =  messages.join(", ")
+  }
+  
+})
